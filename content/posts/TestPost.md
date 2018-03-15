c7f604278e8a8097a88cb23eae21c3e559ffb33e
daeumerj <julian.daeumer@rutgers.edu>
2018-03-15 11:01:42 -0400

    added support for notebook rendering

---
+++ content/posts/TestPost.md
@@ -0,0 +1,4 @@
+Title: Test Post #1
+Date: 2018-03-15
+
+{% notebook downloads/notebooks/test-notebook.ipynb %}
--- pelicanconf.py
+++ pelicanconf.py
@@ -20,16 +20,35 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+#LINKS = (('Pelican', 'http://getpelican.com/'),
+#         ('Python.org', 'http://python.org/'),
+#         ('Jinja2', 'http://jinja.pocoo.org/'),
+#         ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+#SOCIAL = (('You can add links in your config file', '#'),
+#          ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+MARKUP = ['md']
+PLUGIN_PATHS = ['./plugins', './plugins/pelican-plugins']
+PLUGINS = [
+    'summary',       # auto-summarizing articles
+    'feed_summary',  # use summaries for RSS, not full articles
+    'ipynb.liquid',  # for embedding notebooks
+    'liquid_tags.img',  # embedding images
+    'liquid_tags.video',  # embedding videos
+    'liquid_tags.include_code',  # including code blocks
+    'liquid_tags.literal'
+]
+IGNORE_FILES = ['.ipynb_checkpoints']
+
+# for liquid tags
+CODE_DIR = 'downloads/code'
+NOTEBOOK_DIR = 'downloads/notebooks'
+
+ENABLE_MATHJAX = True
